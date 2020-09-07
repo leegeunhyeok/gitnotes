88cd6799caf285ffca81318e5ed26c6a0ab0da1b
leegeunhyeok <dev.ghlee@gmail.com>
2020-09-07 20:17:19 +0900

    ADD: Responsive media query mixin

--- src/App.vue
+++ src/App.vue
@@ -21,24 +21,15 @@ export default defineComponent({
 <style lang="scss">
 @import '@/styles';
 
-html,
-body,
-#app {
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  height: 100%;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-* {
-  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
