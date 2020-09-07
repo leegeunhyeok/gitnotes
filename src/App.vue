838ce0631244d3e1522f5c6e82dd6f6171a09707
leegeunhyeok <dev.ghlee@gmail.com>
2020-09-08 01:30:52 +0900

    UPDATE: font-family style to index.scss
    - TODO: Change to Korean web fonts

--- src/App.vue
+++ src/App.vue
@@ -45,9 +45,6 @@ export default defineComponent({
 @import '@/styles';
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   padding: 0;
