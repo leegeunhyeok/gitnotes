29333cd32ba8d72de310dc7ef39f83a2437da76b
leegeunhyeok <dev.ghlee@gmail.com>
2020-09-10 01:30:52 +0900

    UPDATE: Vuex commit, dispatch payload argument is now optional

--- src/store/index.ts
+++ src/store/index.ts
@@ -12,13 +12,13 @@ const store = createStore({
 export type Store = Omit<VuexStore<State>, 'getters' | 'commit' | 'dispatch'> & {
   commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
     key: K,
-    payload: P,
+    payload?: P,
     options?: CommitOptions,
   ): ReturnType<Mutations[K]>;
 } & {
   dispatch<K extends keyof Actions>(
     key: K,
-    payload: Parameters<Actions[K]>[1],
+    payload?: Parameters<Actions[K]>[1],
     options?: DispatchOptions,
   ): ReturnType<Actions[K]>;
 };
