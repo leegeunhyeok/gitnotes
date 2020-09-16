1b7af96c5e87a7dbcd63c736a2e715e21854f197
leegeunhyeok <dev.ghlee@gmail.com>
2020-09-17 00:58:20 +0900

    UPDATE: Add SUMMARY getter (tags/notes count)

--- src/store/getters/index.ts
+++ src/store/getters/index.ts
@@ -7,21 +7,31 @@ interface UserBasic {
   photo: string;
 }
 
+interface NoteSummary {
+  tagCount: number;
+  noteCount: number;
+}
+
 export enum GetterTypes {
   USER = 'USER',
+  SUMMARY = 'SUMMARY',
   USER_STATE_AVAILABLE = 'USER_STATE_AVAILABLE',
   REPOSITORY_STATE_AVAILABLE = 'REPOSITORY_STATE_AVAILABLE',
 }
 
 export interface Getters<S = State> {
   [GetterTypes.USER](state: S): UserBasic;
+  [GetterTypes.SUMMARY](state: S): NoteSummary;
   [GetterTypes.USER_STATE_AVAILABLE](state: S): boolean;
   [GetterTypes.REPOSITORY_STATE_AVAILABLE](state: S): boolean;
 }
 
 export const getters: GetterTree<State, State> & Getters = {
-  [GetterTypes.USER]({ name, bio, photo }) {
-    return { name, bio, photo };
+  [GetterTypes.USER]({ login, name, bio, photo }) {
+    return { login, name, bio, photo };
+  },
+  [GetterTypes.SUMMARY]({ tags, notes }) {
+    return { tagCount: tags.length, noteCount: notes.length };
   },
   [GetterTypes.USER_STATE_AVAILABLE](state) {
     return !!(state.name && state.token);
